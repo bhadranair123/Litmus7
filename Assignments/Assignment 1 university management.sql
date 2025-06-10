7b51f1adb60e540ec7bc8fe5c752b4f460d39c64
bhadranair123 <137411054+bhadranair123@users.noreply.github.com>
2025-06-10 17:44:04 +0530

    Update Assignment 1 university management.sql

--- Assignments/Assignment 1 university management.sql
+++ Assignments/Assignment 1 university management.sql
@@ -1,34 +1,42 @@
 CREATE TABLE student(
-    stu_id INT PRIMARY KEY,
-    stu_name VARCHAR(40) NOT NULL,
-    stu_email VARCHAR(40) UNIQUE,
-    stu_dob DATE,
-    adv_id INT 
+    student_id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    dob DATE,
+    advisor_id INT 
+    CONSTRAINT CHK_student CHECK(dob<'2021-12-31')
 );
 
 CREATE TABLE advisor(
-    adv_id INT PRIMARY KEY,
-    adv_name VARCHAR(40) NOT NULL,
-    adv_email VARCHAR(40) UNIQUE,
-    specialization VARCHAR(40)
+    advisor_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    specialization VARCHAR(60) DEFAULT "Computer science"
 );
 
 ALTER TABLE student
-ADD FOREIGN KEY (adv_id) 
-REFERENCES advisor(adv_id)
+ADD FOREIGN KEY (advisor_id) 
+REFERENCES advisor(advisor_id)
 ON DELETE SET NULL;
 
 CREATE TABLE course(
-    course_code INT PRIMARY KEY,
-    title VARCHAR(40) NOT NULL,
-    description TEXT,
+    course_code VARCHAR(40) PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    description TEXT ,
     instructor VARCHAR(100)
 );
 
-CREATE TABLE studies(
-    stu_id INT,
-    course_code INT,
-    PRIMARY KEY(stu_id,course_code) ,
-    FOREIGN KEY (stu_id) REFERENCES student(stu_id) ON DELETE CASCADE,
+CREATE TABLE enrollment(
+    student_id VARCHAR(10),
+    course_code VARCHAR(40),
+    status VARCHAR(20) DEFAULT "ENROLLED",
+    enrollment_date DATE,
+    PRIMARY KEY(student_id,course_code) ,
+    CONSTRAINT FK_studentenrollemt
+    FOREIGN KEY (student_id) REFERENCES student(student_id) ON DELETE CASCADE,
+    CONSTRAINT FK_courseenrollment
     FOREIGN KEY (course_code) REFERENCES course(course_code) ON DELETE CASCADE
 );
+
+
+drop table course;
