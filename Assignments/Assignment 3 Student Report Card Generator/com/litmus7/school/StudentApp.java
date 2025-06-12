1c4353d42c5c2c5d7385b1326ba698f3c17f79a4
bhadranair123 <137411054+bhadranair123@users.noreply.github.com>
2025-06-12 23:14:05 +0530

    Create StudentApp.java

---
+++ Assignments/Assignment 3 Student Report Card Generator/com/litmus7/school/StudentApp.java
@@ -0,0 +1,20 @@
+package com.litmus7.school;
+import com.litmus7.school.dto.*;
+import java.util.Scanner;
+
+public class StudentApp {
+	public static void main(String[] a) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("How many students do you want to enter");
+		int number=input.nextInt();
+		Student obj[] = new Student[number];
+		for (int i=0;i<number;i++) {
+			obj[i]=new Student();
+			obj[i].InputDetails();
+		}
+		for (int j=0;j<number;j++)
+			obj[j].PrintReportCard(j);
+		input.close();
+	}
+	
+}
