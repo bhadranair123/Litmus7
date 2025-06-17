585392703f8300f52a7d6a7028e9d3c9de1d5f5d
bhadranair123 <137411054+bhadranair123@users.noreply.github.com>
2025-06-17 20:24:14 +0530

    Update Student.java

--- Assignments/Assignment 3 Student Report Card Generator/com/litmus7/school/dto/Student.java
+++ Assignments/Assignment 3 Student Report Card Generator/com/litmus7/school/dto/Student.java
@@ -1,62 +1,85 @@
 package com.litmus7.school.dto;
+
 import java.util.Scanner;
 
 public class Student {
 	private String name;
-	private int rollno;
-	private int[] marks=new int[5];
-	
-	public void InputDetails(){
-		Scanner scanner= new Scanner(System.in);
+	private int rollNo;
+	private int[] marks = new int[5];
+
+	public enum Grades {
+		A, B, C, D, E, F;
+	}
+
+	public void inputDetails() {
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter student Name: ");
-		name=scanner.nextLine();
+		name = scanner.nextLine();
 		System.out.println("Enter Roll number: ");
-		rollno=scanner.nextInt();
+		rollNo = scanner.nextInt();
 		scanner.nextLine();
 		System.out.println("Enter marks in 5 subjects: ");
-		for(int i=0;i<5;i++) {
-			System.out.println("Subject"+(i+1)+": ");
-			marks[i]=scanner.nextInt();
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Subject" + (i + 1) + ": ");
+			marks[i] = scanner.nextInt();
 			scanner.nextLine();
 		}
-		
+
 	}
-	
-	
-	private int CalculateTotal(int marks[]){
-		int sum=0;
-		for (int i=0;i<5;i++) {
-			sum+=marks[i];
+
+	private int calculateTotal(int marks[]) {
+		int sum = 0;
+		for (int i : marks) {
+			sum += i;
 		}
 		return sum;
 	}
-	
-	
-	private float CalculateAverage(){
-		float average=(float)(CalculateTotal(marks))/5;
+
+	private float calculateAverage() {
+		float average = (float) (calculateTotal(marks)) / 5;
 		return average;
 	}
-	
-	
-	private char GetGrade(int marks[]) {
-		float avg=CalculateAverage();
-		if (avg >= 90 )
-			return 'A';
-		else if (avg>=75)
-			return 'B';
-		else if (avg>=60)
-			return 'C';
-		else if (avg>=50)
-			return 'D';
-		else
-			return 'F';	
-	}
-	
-	public void PrintReportCard(int n) {
-		System.out.println("---Report Card of student "+(n+1)+"---");
-		System.out.println("Name: "+name+"\nRoll Number: "+rollno+"\nTotal Marks: "+CalculateTotal(marks)+"\nAverage Marks: "+CalculateAverage()+"\nGrade: "+GetGrade(marks));
-		
-	}
-	
-}
 
+	private Grades getGrade(int marks[]) {
+		float avg = calculateAverage();
+		if (avg >= 90)
+			return Grades.A;
+		else if (avg >= 75)
+			return Grades.B;
+		else if (avg >= 60)
+			return Grades.C;
+		else if (avg >= 50)
+			return Grades.D;
+		else
+			return Grades.F;
+
+	}
+
+	public void gradeChart(Grades grade) {
+		switch (grade) {
+		case A:
+			System.out.println("Excellent");
+			break;
+		case B:
+			System.out.println("Good");
+			break;
+		case C:
+			System.out.println("Average");
+			break;
+		case D:
+			System.out.println("Pass");
+			break;
+		case F:
+			System.out.println("Failed");
+			break;
+		}
+	}
+
+	public void printReportCard(int n) {
+		System.out.println("---Report Card of student " + (n + 1) + "---");
+		System.out.println("Name: " + name + "\nRoll Number: " + rollNo + "\nTotal Marks: " + calculateTotal(marks)
+				+ "\nAverage Marks: " + calculateAverage() + "\nGrade: " + getGrade(marks));
+		gradeChart(getGrade(marks));
+	}
+
+}
