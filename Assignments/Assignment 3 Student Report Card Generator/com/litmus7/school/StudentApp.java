43dabacf0558d55ce1b3aa6556cacbeb19368c80
bhadranair123 <137411054+bhadranair123@users.noreply.github.com>
2025-06-17 20:23:23 +0530

    Update StudentApp.java

--- Assignments/Assignment 3 Student Report Card Generator/com/litmus7/school/StudentApp.java
+++ Assignments/Assignment 3 Student Report Card Generator/com/litmus7/school/StudentApp.java
@@ -1,20 +1,21 @@
 package com.litmus7.school;
-import com.litmus7.school.dto.*;
+
+import com.litmus7.school.dto.Student;
 import java.util.Scanner;
 
 public class StudentApp {
 	public static void main(String[] a) {
-		Scanner scanner=new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("How many students do you want to enter");
-		int number=scanner.nextInt();
-		Student obj[] = new Student[number];
-		for (int i=0;i<number;i++) {
-			obj[i]=new Student();
-			obj[i].InputDetails();
+		int number = scanner.nextInt();
+		Student students[] = new Student[number];
+		for (int i = 0; i < number; i++) {
+			students[i] = new Student();
+			students[i].inputDetails();
 		}
-		for (int j=0;j<number;j++)
-			obj[j].PrintReportCard(j);
+		for (int j = 0; j < number; j++)
+			students[j].printReportCard(j);
 		scanner.close();
 	}
-	
+
 }
