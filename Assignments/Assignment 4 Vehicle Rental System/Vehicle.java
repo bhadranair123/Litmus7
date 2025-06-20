449aefd6184b80122e3992c5b6b13ec17996e7e3
bhadranair123 <137411054+bhadranair123@users.noreply.github.com>
2025-06-20 12:57:34 +0530

    Create Vehicle.java

---
+++ Assignments/Assignment 4 Vehicle Rental System/Vehicle.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class Vehicle {
+
+	protected String brand;
+	protected String model;
+	protected double rentalPricePerDay;
+
+	protected Vehicle() {
+		this.brand = "Toyota";
+		this.model = "Corolla";
+		this.rentalPricePerDay = 1200;
+	}
+
+	protected Vehicle(String brand, String model, double rentalPricePerDay) {
+		this.brand = brand;
+		this.model = model;
+		this.rentalPricePerDay = rentalPricePerDay;
+	}
+
+	protected void inputDetails() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter brand: ");
+		brand = scanner.nextLine();
+		System.out.println("Enter model: ");
+		model = scanner.nextLine();
+		System.out.println("Enter rental price per day: ");
+		rentalPricePerDay = scanner.nextDouble();
+		scanner.nextLine();
+	}
+
+	protected void displayDetails() {
+		System.out.println("Brand: " + brand);
+		System.out.println("Model: " + model);
+		System.out.println("Rental Price / Day " + rentalPricePerDay);
+	}
+}
