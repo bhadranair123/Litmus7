a1096c798cc1b38a268a37ffcef83c4ab454dbf1
bhadranair123 <137411054+bhadranair123@users.noreply.github.com>
2025-06-24 21:11:36 +0530

    Create Bike.java

---
+++ Assignments/Assignment4 - Upgraded: Vehicle Rental System with File Input and Vehicle Service/Bike.java
@@ -0,0 +1,49 @@
+
+import java.util.Scanner;
+
+public class Bike extends Vehicle {
+
+	protected boolean hasGear;
+	protected int engineCapacity;
+
+	protected Bike() {
+		this("Suzuki", "Gixer", 600.0, false, 125);
+
+	}
+
+	protected Bike(String brand, String model, double rentalPricePerDay, boolean hasGear, int engineCapacity) {
+		super(brand, model, rentalPricePerDay);
+		this.hasGear = hasGear;
+		this.engineCapacity = engineCapacity;
+
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Bike,"+brand+"," +model+ "," + String.valueOf(rentalPricePerDay) + "," + String.valueOf(hasGear)+ String.valueOf(engineCapacity) ;
+	}
+
+	@Override
+	protected void inputDetails() {
+		System.out.println("--Enter Bike Details--");
+		super.inputDetails();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Does it have gears (true/false)? ");
+		hasGear = scanner.nextBoolean();
+		scanner.nextLine();
+		System.out.println("Enter engine capacity(cc): ");
+		engineCapacity = scanner.nextInt();
+		scanner.nextLine();
+	}
+
+	@Override
+	protected void displayDetails() {
+		System.out.println("--Displaying Bike Details--");
+		super.displayDetails();
+		System.out.println("Has Gear: " + hasGear);
+		System.out.println("Engine Capacity: " + engineCapacity + " cc");
+	}
+
+}
