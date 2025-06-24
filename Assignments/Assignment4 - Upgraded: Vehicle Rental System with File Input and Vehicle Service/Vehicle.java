24fc1091f02500d6af5bca983995c0cfda9b71d9
bhadranair123 <137411054+bhadranair123@users.noreply.github.com>
2025-06-24 21:10:35 +0530

    Create Vehicle.java

---
+++ Assignments/Assignment4 - Upgraded: Vehicle Rental System with File Input and Vehicle Service/Vehicle.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+public class Vehicle {
+
+	protected String brand;
+	protected String model;
+	protected double rentalPricePerDay;
+
+	protected Vehicle() {
+		this.brand = "Unknown";
+		this.model = "Unknown";
+		this.rentalPricePerDay = 0.0;
+	}
+
+	protected Vehicle(String brand, String model, double rentalPricePerDay) {
+		this.brand = brand;
+		this.model = model;
+		this.rentalPricePerDay = rentalPricePerDay;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Vehicle," +brand+"," +model+ "," + String.valueOf(rentalPricePerDay);
+	}
+
+	protected void inputDetails() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter brand: ");
+		brand = scanner.nextLine();
+		System.out.println("Enter model: ");
+		model = scanner.nextLine();
+		System.out.println("Enter rental price per day: ");
+		rentalPricePerDay = scanner.nextDouble();
+		scanner.nextLine();
+	}
+
+	protected void displayDetails() {
+		System.out.println("Brand: " + brand);
+		System.out.println("Model: " + model);
+		System.out.println("Rental Price / Day " + rentalPricePerDay);
+	}
+}
